88483834ce84c6560ad898d07825345bdcf99385
R Akash <93132839+AkashRavindran2005@users.noreply.github.com>
2025-11-11 21:47:24 +0530

    Create Two-Integers-sum-II.py

---
+++ Neetcode 150/Arrays and Hashing/Two-Integers-sum-II.py
@@ -0,0 +1,7 @@
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        d = {}
+        for i in range(len(numbers)):
+            if target - numbers[i] in d and target-numbers[i] != numbers[i]:
+                return [d[target-numbers[i]], i+1]
+            d[numbers[i]] = i + 1
