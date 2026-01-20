735e30539314153e10c5f9f1e968026ddae87dd9
R Akash <akashravindran2500@gmail.com>
2026-01-20 23:13:15 +0530

    Implement optimal two-sum solution with two pointers
    Added an optimal solution for the two-sum problem using a two-pointer approach.

--- Neetcode 150/Arrays and Hashing/Two-Integers-sum-II.py
+++ Neetcode 150/Arrays and Hashing/Two-Integers-sum-II.py
@@ -5,3 +5,18 @@ class Solution:
             if target - numbers[i] in d and target-numbers[i] != numbers[i]:
                 return [d[target-numbers[i]], i+1]
             d[numbers[i]] = i + 1
+
+#Optimal Solution
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left = 0
+        right = len(numbers) - 1
+        while left < right:
+            sum = numbers[left] + numbers[right]
+            if sum < target:
+                left += 1
+            if sum > target:
+                right -= 1
+            if sum == target:
+                return [left+1, right+1]
+                
