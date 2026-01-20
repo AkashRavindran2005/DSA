03c506ccd5b1399ca97c844b56967ae75701e332
R Akash <akashravindran2500@gmail.com>
2026-01-20 23:05:25 +0530

    Implement optimal palindrome check with two pointers
    Added an optimal solution for palindrome checking using two pointers.

--- Neetcode 150/Arrays and Hashing/Valid-palindrome.py
+++ Neetcode 150/Arrays and Hashing/Valid-palindrome.py
@@ -5,3 +5,20 @@ class Solution:
             if i.isalnum():
                 newstr += i.lower()
         return newstr == newstr[::-1]
+#Optimal Solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        left = 0
+        right = len(s) - 1
+        while left < right:
+            if not s[left].isalnum():
+                left += 1
+                continue
+            if not s[right].isalnum():
+                right -= 1
+                continue
+            if s[left].lower() != s[right].lower():
+                return False
+            left += 1
+            right -= 1
+        return True
