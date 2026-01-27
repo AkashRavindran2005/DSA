47d09f55aea48f446fcb30a4484b2ee240d50835
AkashRavindran2005 <akashravindran2500@gmail.com>
2026-01-27 23:45:45 +0530

    Permutation in String

---
+++ Neetcode 150/Arrays and Hashing/Permutation-in-string.py
@@ -0,0 +1,17 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        counter = Counter(s1)
+        window = len(s1)
+        matched = 0
+        for i in range(len(s2)):
+            if counter[s2[i]] in counter:
+                counter[s2[i]] -= 1
+                if counter[s2[i]] == 0:
+                    matched += 1
+            if i >= window and s2[i-window] in counter:
+                if counter[s2[i-window]] == 0:
+                    matched -= 1
+                counter[s2[i-window]] += 1
+            if matched == len(counter):
+                return True
+        return False
