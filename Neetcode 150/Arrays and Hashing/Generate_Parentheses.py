3928a7cc84b837acc93c4877130bb91680d9bb3f
R Akash <93132839+AkashRavindran2005@users.noreply.github.com>
2026-03-21 11:43:37 +0530

    Update Generate_Parentheses.py

--- Neetcode 150/Arrays and Hashing/Generate_Parentheses.py
+++ Neetcode 150/Arrays and Hashing/Generate_Parentheses.py
@@ -11,3 +11,17 @@ class Solution:
                 backtrack(curr + ")", open_cnt, close_cnt + 1)
         backtrack("", 0, 0)
         return res
+#2nd way
+
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        res = [[] for _ in range(n+1)]
+        res[0] = [""]
+
+        for k in range(n + 1):
+            for i in range(k):
+                for left in res[i]:
+                    for right in res[k-i-1]:
+                        res[k].append("(" + left + ")" + right)
+
+        return res[-1]
